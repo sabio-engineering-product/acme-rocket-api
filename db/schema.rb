bad8076b1b5ff5c1630fc84029efab11ed8e6500
Alasdair Miller <amiller@sabiogroup.com>
2023-07-20 14:27:52 +0000

    feat: add new feature AC-21
    Added database config

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,37 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_601_122_719) do
-  create_table 'astronauts', force: :cascade do |t|
-    t.string 'name'
-    t.string 'nationality'
-    t.date 'date_of_birth'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_122719) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "astronauts", force: :cascade do |t|
+    t.string "name"
+    t.string "nationality"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'astronauts_launches', id: false, force: :cascade do |t|
-    t.integer 'astronaut_id', null: false
-    t.integer 'launch_id', null: false
+  create_table "astronauts_launches", id: false, force: :cascade do |t|
+    t.bigint "astronaut_id", null: false
+    t.bigint "launch_id", null: false
   end
 
-  create_table 'launches', force: :cascade do |t|
-    t.string 'mission_name'
-    t.datetime 'launch_date'
-    t.string 'launch_site'
-    t.integer 'rocket_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['rocket_id'], name: 'index_launches_on_rocket_id'
+  create_table "launches", force: :cascade do |t|
+    t.string "mission_name"
+    t.datetime "launch_date"
+    t.string "launch_site"
+    t.bigint "rocket_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rocket_id"], name: "index_launches_on_rocket_id"
   end
 
-  create_table 'rockets', force: :cascade do |t|
-    t.string 'name'
-    t.string 'model'
-    t.string 'manufacturer'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "rockets", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.string "manufacturer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'launches', 'rockets'
+  add_foreign_key "launches", "rockets"
 end
